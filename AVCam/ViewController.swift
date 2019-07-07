6d7650be3592077fc528c503f73370acce04404c
Ajo M Varghese <ajo@jupitersupplystation.com>
2019-07-07 06:47:18 +0530

    Initial commit

---
+++ AVCam/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AVCam
+//
+//  Created by Ajo M Varghese on 07/07/19.
+//  Copyright © 2019 Ajo M Varghese. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
