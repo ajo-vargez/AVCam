cc6f6dbc94bacd1f6d56e559f3596bad8d83efcb
Ajo M Varghese <ajo@jupitersupplystation.com>
2019-07-07 07:12:12 +0530

    Added some gitignore files

---
+++ AVCam/View/MainView.swift
@@ -0,0 +1,13 @@
+//
+//  MainView.swift
+//  AVCam
+//
+//  Created by Ajo M Varghese on 07/07/19.
+//  Copyright © 2019 Ajo M Varghese. All rights reserved.
+//
+
+import Foundation
+
+class MainView {
+
+}
