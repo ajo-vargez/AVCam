c3aab13cf0ae6c18d7b9bbfc8ca2cb53c9b31909
Ajo M Varghese <ajo@jupitersupplystation.com>
2019-07-07 08:50:05 +0530

    Setting up the initial view

--- AVCam/Controller/MainVC.swift
+++ AVCam/Controller/MainVC.swift
@@ -11,9 +11,25 @@ import AVFoundation
 
 class MainVC: UIViewController {
 
+  // MARK:- Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    mainView = MainView(frame: (self.window?.frame)!)
+    self.view.addSubview(mainView)
   }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    navigationController?.setNavigationBarHidden(true, animated: false)
+  }
+
+
+
+  // MARK:- Private
+  private var window: UIWindow?
+  private var mainView: MainView!
 }
 
--- AVCam/View/MainView.swift
+++ AVCam/View/MainView.swift
@@ -6,8 +6,54 @@
 //  Copyright © 2019 Ajo M Varghese. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-class MainView {
+class MainView: UIView {
 
+  // MARK:- Lifecycle
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    setupViews()
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+
+
+
+  // MARK:- SetupUI
+
+  // Camera's viewFinder
+  let previewView: UIView = {
+    let view = UIView()
+    view.backgroundColor = .red
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+
+  let bottomControls: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+
+  /// Sets the UI for the given view
+  private func setupViews() {
+    addSubview(previewView)
+    addSubview(bottomControls)
+
+    // Preview
+    NSLayoutConstraint.activate([previewView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                                previewView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                                previewView.topAnchor.constraint(equalTo: self.topAnchor, constant: 34),
+                                previewView.bottomAnchor.constraint(equalTo: bottomControls.topAnchor)])
+
+    // Bottom Controls
+    NSLayoutConstraint.activate([bottomControls.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+                                bottomControls.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+                                bottomControls.heightAnchor.constraint(equalToConstant: 114),
+                                bottomControls.bottomAnchor.constraint(equalTo: self.bottomAnchor)])
+  }
 }
