be00c5e12bafa4b04fcb7e1f41831e7d571c4750
Ajo M Varghese <mal.01ajo@yahoo.com>
2020-10-15 15:14:17 +0530

    Edit the year in launch screen and add a camera controls view file

---
+++ AVCam/View/CameraControlsView.swift
@@ -0,0 +1,24 @@
+//
+//  CameraControls.swift
+//  AVCam
+//
+//  Created by Ajo on 15/10/20.
+//  Copyright © 2020 Ajo M Varghese. All rights reserved.
+//
+
+import UIKit
+
+
+class CameraControlsView: UIView {
+	
+	// Mark:- Lifecycle
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		
+		// TODO:- Create the shutter button and other components in here.
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
